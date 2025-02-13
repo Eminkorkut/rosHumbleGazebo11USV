721653b816d09b2ec2ea0526bae66f610c8589f9
Emin KORKUT <eminkrkut@gmail.com>
2025-02-13 21:33:14 +0300

    Update autonomous_boat_movement.py

--- src/autonomous_boat_movement.py
+++ src/autonomous_boat_movement.py
@@ -7,79 +7,86 @@ import rclpy
 import cv2
 
 
-class VesselController(Node):
+class usvController(Node):
     def __init__(self):
-        super().__init__('vesselController')
-        # gemi hareketlerini yayınlamak için bir yayıncı oluştur
-        self.publisher_ = self.create_publisher(Twist, '/vessel_a/cmd_vel', 10)
-        # kameradan görüntü almak için bir abone oluştur
+        super().__init__('usvController')
+
+        # create a publisher to publish vessel movements
+        self.publisher = self.create_publisher(Twist, '/vessel_a/cmd_vel', 10)
+
+        # create a subscriber to get image from camera
         self.subscription = self.create_subscription(
             Image,
             '/vessel_a/camera/image_raw',
-            self.imageCallback,
+            self.imageCallBack,
             10
         )
-        # cv2 kütüphanesini kullanmak için bir nesne oluştur
+
+        # create an object to use cv2 library
         self.bridge = CvBridge()
-        # YOLO modelini yükle
+        # load YOLO model
         self.model = YOLO('weights/230_epochs/weights/best.pt')
-        
-        # gemi hareketleri için değişkenler
+
+
+        # variables for vessel movements
         self.currentVelocity = 0.1
         self.currentAngularVelocity = 0.0
         self.appliedForce = 0.0
         self.appliedTorque = 0.0
-        # arac kütlesi
+        # mass of the vehicle
         self.mass = 1.0
-        # atalet momenti
+        # inertia of the vehicle
         self.inertia = 1.0
-        # zaman adımı (frekansı)
-        self.dt = 0.1
+        # time step (frequency)
+        self.timeStep = 0.1
 
-        # zamanlayıcı oluştur
-        self.timer = self.create_timer(self.dt, self.update)
+
+        self.timer = self.create_timer(self.timeStep, self.update)
+
+
+        self.targetHistory = [] 
         self.lastTargetPair = None
-        self.targetHistory = []  # Hedef geçmişi
-        self.alpha = 0.2  # EMA ağırlık katsayısı
 
         self.prevErrorX = 0.0
         self.integralX = 0.0
-        # pid yapisi
-        self.KpAngular = 0.002  # Daha düşük kazanç
-        self.KiAngular = 0.0001  # Entegre kazancı
-        self.KdAngular = 0.001  # Türev kazancı
+        self.alpha = 0.2
+
+
+        # pid parameters
+        self.kpAngular = 0.002 # proportional gain
+        self.kiAngular = 0.0001 # integral gain
+        self.kdAngular = 0.001 # derivative gain 
+
 
     def update(self):
-        # hız ve yön güncelleme     
         acceleration = self.appliedForce / self.mass
         angularAcceleration = self.appliedTorque / self.inertia
 
-        # ivme hesabı
-        self.currentVelocity += acceleration * self.dt
-        self.currentAngularVelocity += angularAcceleration * self.dt
+        # calculate the velocity
+        self.currentVelocity += acceleration * self.timeStep
+        self.currentAngularVelocity += angularAcceleration * self.timeStep
 
-        # yeni hiz belirleleme
-        damping = 0.95  # Daha yüksek sönümleme faktörü
-        self.currentVelocity *= damping
-        self.currentAngularVelocity *= damping
+        # create a Twist message to publish the vessel movements
+        msg = Twist()
+        msg.linear.x = self.currentVelocity
+        msg.angular.z = self.currentAngularVelocity
+        self.publisher.publish(msg)
+        
 
-        # ros2 mesajı yayinla
-        twist = Twist()
-        twist.linear.x = self.currentVelocity
-        twist.angular.z = self.currentAngularVelocity
-        self.publisher_.publish(twist)
 
-    def adjustMovement(self, avgX, refX, avgY, refY):
-        # hatayi hesapla
-        errorX = avgX - refX
-        self.integralX += errorX * self.dt
-        derivative_x = (errorX - self.prevErrorX) / self.dt
+    
+    def adjustMovement(self, targetX, referenceX, targetY, referenceY):
+        # calculate the error
+        errorX = targetX - referenceX
+        self.integralX += errorX * self.timeStep
+        derivativeX = (errorX - self.prevErrorX) / self.timeStep
 
-        # PID ile verilmesi gereken kuvvet
-        self.appliedTorque = -(self.KpAngular * errorX + self.KiAngular * self.integralX + self.KdAngular * derivative_x)
-        self.appliedForce = 0.01 * (avgY - refY)
+        # calculate the angular velocity with PID controller
+        self.appliedTorque = -(self.kpAngular * errorX + self.kiAngular * self.integralX + self.kdAngular * derivativeX)
+        self.appliedForce = 0.01 * (targetY - referenceY)
 
-        # Yön hatasını sınırlandırma (örn. -1 ile 1 arası)
+
+        # apply limits to the angular velocity
         if self.appliedTorque > 1.0:
             self.appliedTorque = 1.0
         elif self.appliedTorque < -1.0:
@@ -87,106 +94,105 @@ class VesselController(Node):
 
         self.prevErrorX = errorX
 
-    def imageCallback(self, msg):
-        # Görüntüyü al
+    def imageCallBack(self, msg):
+        # convert image message to cv2 image
         frame = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
-        # YOLO modelini kullanarak nesneleri algıla
+        # detect objects in the image
         results = self.model.predict(frame, max_det=5, iou=0.6)
 
-        # Geminin orta noktasını al
-        refX, refY = frame.shape[1] // 2, (frame.shape[0] // 2 + 50)
-        detections = []
+        # get the center of the main boat
+        mainBoatCenterX = frame.shape[1] // 2
+        mainBoatCenterY = frame.shape[0] // 2
+
+        # list to store detected objects
+        detectionObjectCenterList = []
 
         for result in results:
             boxes = result.boxes
             for box in boxes:
                 x1, y1, x2, y2 = map(int, box.xyxy[0])
-                cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
-                detections.append((cx, cy))
+                detectedObjectCenterX = (x1 + x2) // 2
+                detectedObjectCenterY = (y1 + y2) // 2
+                detectionObjectCenterList.append((detectedObjectCenterX, detectedObjectCenterY))
+
                 
-                # Algılanan kutuları çiz
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                # Orta noktaları işaretle
-                cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
+                cv2.circle(frame, (detectedObjectCenterX, detectedObjectCenterY), 5, (0, 0, 255), -1)
 
-        if len(detections) >= 2:
-            # Y değerleri arasındaki farkı kontrol et
-            max_y_diff = 50  # Y değerleri arasındaki maksimum fark (yanlis hedefleme durumunu azaltma)
-            # Y değerine göre sırala (bize en yakin dubalar y degeri olarak daha buyuktur)
-            detections.sort(key=lambda x: x[1], reverse=True)
 
-            # belirlenen dubalar arasinda y degeri kontrolu
-            if abs(detections[0][1] - detections[1][1]) < max_y_diff:
-                self.lastTargetPair = detections[0], detections[1]
+        if len(detectionObjectCenterList) >= 2:
+            # maximum distance between the detected objects
+            maxYdifference = 50
 
-                # listenin bos olup olmadigini kontrol et
+            # sort the detected objects according to their y coordinates
+            detectionObjectCenterList.sort(key=lambda x: x[1], reverse=True)
+
+            if abs(detectionObjectCenterList[0][1] - detectionObjectCenterList[1][1]) < maxYdifference:
+                self.lastTargetPair = detectionObjectCenterList[0], detectionObjectCenterList[1]
+
+
                 if self.targetHistory:
-                    prev_x, prev_y = self.targetHistory[-1]
-                    avgX = self.alpha * ((detections[0][0] + detections[1][0]) // 2) + (1 - self.alpha) * prev_x
-                    avgY = self.alpha * ((detections[0][1] + detections[1][1]) // 2) + (1 - self.alpha) * prev_y
-                # hedefin orta noktasini al
+                    prevX, prevY = self.targetHistory[-1]
+                    avgX = self.alpha * ((detectionObjectCenterList[0][0] + detectionObjectCenterList[1][0]) // 2) + (1 - self.alpha) * prevX
+                    avgY = self.alpha * ((detectionObjectCenterList[0][1] + detectionObjectCenterList[1][1]) // 2) + (1 - self.alpha) * prevY
                 else:
-                    avgX = (detections[0][0] + detections[1][0]) // 2
-                    avgY = (detections[0][1] + detections[1][1]) // 2
+                    avgX = (detectionObjectCenterList[0][0] + detectionObjectCenterList[1][0]) // 2
+                    avgY = (detectionObjectCenterList[0][1] + detectionObjectCenterList[1][1]) // 2
 
-                # hedef gecmisine hedef ekle
                 self.targetHistory.append((avgX, avgY))
 
-                # belirli sayida gecmis tut
                 if len(self.targetHistory) > 10:
                     self.targetHistory.pop(0)
-            
-            # belirlenen dubalar y araliginin disinda ise son hedefe git
             else:
-                avgX, avgY = refX, refY
+                avgX, avgY = mainBoatCenterX, mainBoatCenterY
         
-        # Bir hedef varsa, hedefe git
-        elif len(detections) == 1:
+        elif len(detectionObjectCenterList) == 1:
             if self.targetHistory:
                 avgX, avgY = self.targetHistory[-1]
             else:
-                avgX, avgY = refX, refY
-        # Hedef yoksa, son hedefe git
+                avgX, avgY = mainBoatCenterX, mainBoatCenterY
+        
         else:
             if self.targetHistory:
                 avgX, avgY = self.targetHistory[-1]
             else:
-                avgX, avgY = refX, refY
+                avgX, avgY = mainBoatCenterX, mainBoatCenterY
 
-        # Hareketi ayarla
-        self.adjustMovement(avgX, refX, avgY, refY)
 
-        # Hedeflenen kutular belirli şartlara uygunsa hedefi çiz
-        if len(detections) >= 2 and abs(detections[0][1] - detections[1][1]) < max_y_diff:
+        # adjust the movement of the boat
+        self.adjustMovement(avgX, mainBoatCenterX, avgY, mainBoatCenterY)
+
+        # draw the line between the main boat and the target
+        if len(detectionObjectCenterList) >= 2 and abs(detectionObjectCenterList[0][1] - detectionObjectCenterList[1][1]) < maxYdifference:
             cv2.circle(frame, (int(avgX), int(avgY)), 7, (255, 0, 0), -1)
-            cv2.line(frame, (refX, refY), (int(avgX), int(avgY)), (0, 255, 255), 2)
+            cv2.line(frame, (mainBoatCenterX, mainBoatCenterY), (int(avgX), int(avgY)), (0, 255, 255), 2)
             if self.lastTargetPair:
                 cv2.line(frame, self.lastTargetPair[0], self.lastTargetPair[1], (0, 0, 255), 2)
-        
-        # Geminin ortasını çiz
-        cv2.circle(frame, (refX, refY), 7, (255, 255, 0), -1)
 
-        # Görüntüyü göster
+
+        # draw the center of the main boat
+        cv2.circle(frame, (mainBoatCenterX, mainBoatCenterY), 5, (255, 0, 0), -1)
+
+        # show the frame
         cv2.imshow('frame', frame)
         cv2.waitKey(1)
 
-
-    # node'u kapat
-    def destroy_node(self):
+    def destroynode(self):
         super().destroy_node()
         cv2.destroyAllWindows()
 
 
-# main fonksiyonu
+# main function to run the node
 def main(args=None):
     rclpy.init(args=args)
-    vessel_controller = VesselController()
+    controller = usvController()
     try:
-        rclpy.spin(vessel_controller)
+        rclpy.spin(controller)
     finally:
-        vessel_controller.destroy_node()
+        controller.destroynode()
         rclpy.shutdown()
 
-# main fonksiyonunu çalıştır
+# if the script is run directly, execute the main function
 if __name__ == '__main__':
     main()
+
